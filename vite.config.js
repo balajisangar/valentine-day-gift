4f993f02a5fd48230f71767e7e0a7f960ec1ed7d
balajisangar <balajisangar.in@gmail.com>
2026-02-17 16:07:57 +0530

    Deploy build files for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/valentine-day-gift-main/",
   plugins: [react()],
+  base: '/valentine-day-gift/',
 })
