33b0baf830a9606f8f4f665f3cdd6638461d996e
balajisangar <balajisangar.in@gmail.com>
2026-02-17 15:35:36 +0530

    Build proposal for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/valentine-day-gift/",
+  base: "/valentine-day-gift-main/",
   plugins: [react()],
 })
